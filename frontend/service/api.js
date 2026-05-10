3cdf7a1cc5e8025c7fef457e3ddb371900784f1a
Deepak Singh <ds1090429@gmail.com>
2026-05-10 21:40:50 +0530

    fix env file in frontend

--- frontend/service/api.js
+++ frontend/service/api.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_LOCAL_API
+    : import.meta.env.VITE_PROD_API;
+
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
-  withCredentials: true, 
+  baseURL: BASE_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export default API
-
+export default API;
