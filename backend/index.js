58257c69f10a1bc7500e89062e0f235ab09aa1e9
Deepak Singh <ds1090429@gmail.com>
2026-05-10 22:02:11 +0530

    Fix vercel.json and cors error

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://note-app-backend-zeta-six.vercel.app",
+    ],
     credentials: true,
   })
 );
